946d26949db3c7f8baed8ad71f0288325d3c470b
Pedro Ribeiro <pedro@workwithgrid.com>
2021-01-01 11:57:31 -0500

    to pug

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ var gulp          = require('gulp');
 var browserSync   = require('browser-sync').create();
 var $             = require('gulp-load-plugins')();
 var autoprefixer  = require('autoprefixer');
+var pug = require('gulp-pug');
 
 var sassPaths = [
   'node_modules/foundation-sites/scss',
@@ -22,15 +23,25 @@ function sass() {
     .pipe(browserSync.stream());
 };
 
+function buildHTML() {
+  return gulp.src('views/*.pug')
+  .pipe(pug({
+    // Your options in here.
+  }))
+  .pipe(gulp.dest('.'))
+
+}
+
 function serve() {
   browserSync.init({
     server: "./"
   });
 
   gulp.watch("scss/*.scss", sass);
+  gulp.watch("views/*.pug", buildHTML);
   gulp.watch("*.html").on('change', browserSync.reload);
 }
-
+gulp.task('views', buildHTML);
 gulp.task('sass', sass);
 gulp.task('serve', gulp.series('sass', serve));
-gulp.task('default', gulp.series('sass', serve));
+gulp.task('default', gulp.series('sass', 'views', serve));
